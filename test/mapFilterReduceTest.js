0c820859a96b9b6124de0f8abe89d0153a3cf913
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-11-20 18:39:26 +0530

    Modified test framework to mocha test framework.

--- test/mapFilterReduceTest.js
+++ test/mapFilterReduceTest.js
@@ -10,6 +10,10 @@ const incrementByOne = function(number) {
   return ++number;
 }
 
+const append = function(string) {
+  return string + " " + "hello";
+}
+
 const changeCase = function(element) {
   return element.toUpperCase();
 }
@@ -22,44 +26,68 @@ const add = function(num1, num2) {
   return num1 + num2;
 }
 
-const append = function(string) {
-  return string + " " + "hello";
-}
-
-const isAboveThreshold = function(number) {
-  return number > 3;
+const isAboveThreshold = function(threshold) {
+  return function(number){
+    return number > threshold;
+  }
 }
 
 const concat = function(element1, element2) {
   return element1 + element2;
 }
 
-const testMap = function() {
-  assert.deepEqual(map(incrementByOne, []),[]);
-  assert.deepEqual(map(incrementByOne, [1]),[2]);
-  assert.deepEqual(map(incrementByOne, [1,2,3]),[2,3,4]);
-  assert.deepEqual(map(append, ["hey", "this"]), ["hey hello", "this hello"]);
-  assert.deepEqual(map(changeCase, ["hey", "this"]), ["HEY", "THIS"]);
-  console.log("map function test passed");
-}
+describe("map", function() {
+  let strings = ["hey", "this"];
 
-const testFilter = function() {
-  assert.deepEqual(filter(isEven, []),[]);
-  assert.deepEqual(filter(isEven, [1]),[]);
-  assert.deepEqual(filter(isEven, [1,2,3,4,5,6]),[2,4,6]);
-  assert.deepEqual(filter(isAboveThreshold, [1,2,3,4,5,6]),[4,5,6]);
-  console.log("filter function test passed");
-}
+  it("should return empty array", function() {
+    assert.deepEqual(map(incrementByOne, []),[]);
+  });
 
-const testReduce = function() {
-  assert.equal(reduce(add, undefined, [1]),1);
-  assert.equal(reduce(add, undefined, [1,2,3,4,5,6]),21);
-  assert.equal(reduce(add, 4,[1,2,3,4,5,6]), 25);
-  assert.equal(reduce(concat, undefined, ["h", "e", "l", "l", "o"]), ["hello"]);
-  assert.equal(reduce(concat, "K", ["h", "e", "l", "l", "o"]), ["Khello"]);
-  console.log("reduce function test passed");
-}
+  it("should increment each element by one", function() {
+    assert.deepEqual(map(incrementByOne, [1]),[2]);
+    assert.deepEqual(map(incrementByOne, [1,2,3]),[2,3,4]);
+  });
 
-testMap();
-testFilter();
-testReduce();
+  it("should append hello to each strings", function() {
+    assert.deepEqual(map(append, strings), ["hey hello", "this hello"]);
+  });
+
+  it("should change case of strings to upper case", function() {
+    assert.deepEqual(map(changeCase, strings), ["HEY", "THIS"]);
+  });
+
+});
+
+describe("filter", function() {
+  it("should return empty array", function() {
+    assert.deepEqual(filter(isEven, []),[]);
+  });
+
+  it("should return even an array of even numbers", function() {
+    assert.deepEqual(filter(isEven, [1]),[]);
+    assert.deepEqual(filter(isEven, [1,2,3,4,5,6]),[2,4,6]);
+  });
+
+  it("should return an array of numbers above a threshold", function() {
+    assert.deepEqual(filter(isAboveThreshold(3), [1,2,3,4,5,6]),[4,5,6]);
+  });
+
+});
+
+describe("reduce", function() {
+  it("should return the sum of elements of an array", function() {
+    assert.equal(reduce(add, undefined, [1]),1);
+    assert.equal(reduce(add, undefined, [1,2,3,4,5,6]),21);
+  });
+
+  it("should add all elements allong with initial value", function() {
+    assert.equal(reduce(add, 4,[1,2,3,4,5,6]), 25);
+  });
+
+  it("should concat all the elements", function() {
+    assert.equal(reduce(concat, undefined, ["h", "e", "l", "l", "o"]), "hello");
+    assert.equal(reduce(concat, "K", ["h", "e", "l", "l", "o"]), "Khello");
+  });
+});
+
+
