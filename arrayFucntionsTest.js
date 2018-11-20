a2556a0d94f6abad7994b18c529f94125c6fc367
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-11-20 12:15:11 +0530

    Introduce reduce function.
    The function passes two elements at a time to a reducer and reduces them
    to a single output.

--- arrayFucntionsTest.js
+++ arrayFucntionsTest.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const lib = require('./arrayFunctions');
 const { 
   map, 
-  filter
+  filter,
+  reduce
 } = lib;
 
 const incrementByOne = function(number) {
@@ -13,11 +14,15 @@ const isEven = function(number) {
   return number % 2 === 0;
 }
 
+const add = function(num1, num2) {
+  return num1 + num2;
+}
 const testMap = function() {
   assert.deepEqual(map([],incrementByOne),[]);
   assert.deepEqual(map([1],incrementByOne),[2]);
   assert.deepEqual(map([1,2,3],incrementByOne),[2,3,4]);
   assert.deepEqual(map([-1,-2,-3],incrementByOne),[0,-1,-2]);
+  console.log("map function test passed");
 }
 
 const testFilter = function() {
@@ -25,7 +30,16 @@ const testFilter = function() {
   assert.deepEqual(filter([1],isEven),[]);
   assert.deepEqual(filter([1,2,3,4,5,6],isEven),[2,4,6]);
   assert.deepEqual(filter([-1,-2,-3,-4,-5,-6],isEven),[-2,-4,-6]);
+  console.log("filter function test passed");
+}
+
+const testReduce = function() {
+  assert.equal(reduce([1], add),1);
+  assert.equal(reduce([1,2,3,4,5,6], add),21);
+  assert.equal(reduce([1,2,3,4,5,6], add, 4), 25);
+  console.log("reduce function test passed");
 }
 
 testMap();
 testFilter();
+testReduce();
--- arrayFunctions.js
+++ arrayFunctions.js
@@ -16,8 +16,26 @@ const filter = function(elements, predicate) {
   return processedElements;
 }
 
+const reduce = function(elements, reducer, initializer) {
+  let result = elements[0];
+  let position = 1;
+
+  if(initializer){
+    result = initializer;
+    position = 0;
+  }
+
+  while(position < elements.length){
+    result = reducer(result, elements[position]);
+    position++;
+  }
+
+  return result;
+}
+
 module.exports = {
   map,
-  filter
+  filter,
+  reduce
 }
  
